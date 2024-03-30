97be849489cb888f23586927ea7c9e8cffd616aa
shi-n <shi-n_xyz@cloudsquare.jp>
2024-03-30 20:16:44 +0900

    MyProduct追加

---
+++ Calendar Analysis/MyProductView.swift
@@ -0,0 +1,18 @@
+//
+//  MyProductView.swift
+//  Calendar Analysis
+//
+//  Created by Shin Inaba on 2024/03/30.
+//
+
+import SwiftUI
+
+struct MyProductView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyProductView()
+}
